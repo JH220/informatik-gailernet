43f8f6b7e72ef98ef6d9e0e5c73069dfbd5a848f
JH220 aka. Jan-Hendrik <50051569+JH220@users.noreply.github.com>
2021-04-21 14:54:50 +0200

    Update

---
+++ src/de/gailernet/bedingteanweisungenundschleifen/dieifanweisung/NonFettKalkulator.java
@@ -0,0 +1,37 @@
+package de.gailernet.bedingteanweisungenundschleifen.dieifanweisung;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class NonFettKalkulator {
+    public static void main(String[] args) {
+        double apriceperpound;
+        double bpriceperpound;
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Preis pro Pfund Pakcung A: ");
+            double aprice = scanner.nextDouble();
+            System.out.print("Prozent mageres Fleisch Packung A: ");
+            int apercent = scanner.nextInt();
+            apriceperpound = aprice / apercent;
+
+            System.out.print("Preis pro Pfund Pakcung B: ");
+            double bprice = scanner.nextDouble();
+            System.out.print("Prozent mageres Fleisch Packung B: ");
+            int bpercent = scanner.nextInt();
+            bpriceperpound = bprice / bpercent;
+        } catch (InputMismatchException exception) {
+            System.err.println("Bitte gebe eine richtige Zahl ein!");
+            return;
+        }
+
+        System.out.println("Packung A kostet pro Pfund mageres Fleisch: " + (apriceperpound * 100));
+        System.out.println("Packung B kostet pro Pfund mageres Fleisch: " + (bpriceperpound * 100));
+
+        if(apriceperpound < bpriceperpound) System.out.println("Packung A ist preiswerter.");
+        else if (apriceperpound > bpriceperpound) System.out.println("Packung B ist preiswerter.");
+        else System.out.println("Beide Packungen sind gleich preiswert.");
+    }
+}
---
+++ src/de/gailernet/bedingteanweisungenundschleifen/dieifanweisung/Y2KProblemDetector.java
@@ -0,0 +1,33 @@
+package de.gailernet.bedingteanweisungenundschleifen.dieifanweisung;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Y2KProblemDetector {
+    public static void main(String[] args) {
+        int birthyear;
+        int currentyear;
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Geburtsjahr: ");
+            birthyear = scanner.nextInt();
+            try {
+                System.out.print("Aktuelles Jahr: ");
+                currentyear = scanner.nextInt();
+            } catch (InputMismatchException exception) {
+                currentyear =Integer.valueOf(new SimpleDateFormat("yy").format(new Date()));
+            }
+        } catch (InputMismatchException exception) {
+            System.err.println("Bitte gebe eine richtige Zahl ein!");
+            return;
+        }
+
+        System.out.println("Ihr Alter ist: " + (currentyear - birthyear));
+    }
+}
